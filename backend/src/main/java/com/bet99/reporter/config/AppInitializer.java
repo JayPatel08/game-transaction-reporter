4dd3c991aee8e7a7e6b9609fb5eb2ea8c30b7a2e
JayPatel08 <jaypatel886616@gmail.com>
2026-08-05 01:09:17 -0400

    quick fix

--- backend/src/main/java/com/bet99/reporter/config/AppInitializer.java
+++ backend/src/main/java/com/bet99/reporter/config/AppInitializer.java
@@ -11,7 +11,7 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class<?>[] { WebConfig.class };
+        return new Class<?>[] { WebMvcConfig.class };
     }
 
     @Override
